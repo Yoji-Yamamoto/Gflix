312480b56c162dfed04c679ed75eef2a037fc3eb
Yoji Yamamoto <yyouji777@gmail.com>
2020-02-12 18:02:31 +0900

    whether-deploy-can-success

--- includes/config.php
+++ includes/config.php
@@ -2,20 +2,23 @@
     ob_start();
     session_start();
     date_default_timezone_set("Asia/Tokyo");
-    
-    function dbConnect(){
-        $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-        $db['dbname'] = ltrim($db['path'], '/');
-        $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-        $user = $db['user'];
-        $password = $db['pass'];
-        $options = array(
-          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-          PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-        );
-        $dbh = new PDO($dsn,$user,$password,$options);
-        return $dbh;
-      }
+
+    try{
+      //データベースへの接続
+      $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $cleardb_server   = $cleardb_url["host"];
+      $cleardb_username = $cleardb_url["user"];
+      $cleardb_password = $cleardb_url["pass"];
+      $cleardb_db       = substr($cleardb_url["path"],1);
+      $con = new PDO("mysql:dbname=$cleardb_url;host=$cleardb_server", "$cleardb_username","$cleardb_password");
+      /*    ローカル環境でのdb接続
+            $con = new PDO("mysql:dbname=Gflix;host=localhost", "root","root");
+      */
+      //PDOのエラーレポート
+      $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+  }
+  catch (PDOException $e){
+      exit("Connection failed: " . $e->getMessage());
+  }
 
 ?>
