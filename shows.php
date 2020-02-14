0f6e0966900e2da8f9fce5dda24021f0f0fd96bc
Yoji Yamamoto <yyouji777@gmail.com>
2020-02-14 11:10:18 +0900

    modified

--- shows.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("includes/header.php");
-    $preview = new PreviewProvide($con, $userLoggedIn);
-    echo $preview->createTvPreview(null);
-
-    $category = new Category($con, $userLoggedIn);
-    echo $category->showtvShowCategories();
-
-?>
